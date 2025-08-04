42f8cf88bc2ee1ca7c0fd12140c65de7a755a1cd
Nate Spencer <48964709+natespencer28@users.noreply.github.com>
2025-08-03 20:43:00 -0600

    Update and rename WN11-CC-000280 to WN11-CC-000280.ps1

---
+++ STIGs/WN11-CC-000280.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script controls the ability of users to supply passwords automatically as part of their remote desktop connection
+
+.NOTES
+    Author          : Nate Spencer
+    LinkedIn        : 
+    GitHub          : https://www.linkedin.com/in/nathaniel-spencer-133903153/
+    Date Created    : 2025-08-03
+    Last Modified   : 2024-08-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000280
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
