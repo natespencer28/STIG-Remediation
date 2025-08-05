7e9ea2ddb41624fc4a40d08b6968c85a0fa4cf53
Nate Spencer <48964709+natespencer28@users.noreply.github.com>
2025-08-04 22:30:37 -0600

    Update and rename WN11-CC-000315.ps1 to WN11-CC-000290.ps1

---
+++ STIGs/WN11-CC-000290.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    This PowerShell script controls the ability of users to supply passwords automatically as part of their remote desktop connection
+
+.NOTES
+    Author          : Nate Spencer
+    LinkedIn        : 
+    GitHub          : https://www.linkedin.com/in/nathaniel-spencer-133903153/
+    Date Created    : 2025-08-03
+    Last Modified   : 2024-08-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000280
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+#>
+
+# YOUR CODE GOES HERE
+
+# Set the registry path
+# Define the registry path
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+
+# Ensure the registry key exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the MinEncryptionLevel to 3 (which typically means "High" encryption)
+New-ItemProperty -Path $regPath -Name "MinEncryptionLevel" -PropertyType DWord -Value 3 -Force
+
+Write-Host "Registry value 'MinEncryptionLevel' set to 3 successfully."
